25af6a5b65d356b6f52213b3658ee9e80a4c16f5
Iamneo <techadmin@iamneo.ai>
2023-08-26 11:36:43 +0000

    Backup Commit

--- angularapp/src/app/currencyconv.pipe.ts
+++ angularapp/src/app/currencyconv.pipe.ts
@@ -1,31 +1,63 @@
-import { Component } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Pipe({
+  name: 'currencyconv'
 })
-export class AppComponent {
-  title = 'pipes';
-  fromCurr :String = '';
-  toCurr   :string = '';
-  amount !:number;
-  result: string = 'sample';
-  show:boolean = false;
+export class CurrencyconvPipe implements PipeTransform {
+
+  transform(value: number, from:String,to:String): any {
+
+    if(from == to)
+    {
+      return value;
+    }
+
+    if(from == '0.876893')
+    {
+      from = 'GBP';
+    }
+    else if (from == '1.126735') 
+    {
+      from = 'USD';
+    } 
+    else {
+      from = 'INR';
+    }
+
+    if (to == '0.876893') 
+    {
+      to = 'GBP';
+    } 
+    else if (to == '1.126735') 
+    {
+      to = 'USD';
+    } 
+    else 
+    {
+      to = 'INR';
+    }
+
+    interface Exchange {
+      id :String;
+      value :number;
+    }
+
+    const exchangeRates: Exchange[] = 
+    [
+      {id: 'USD',  value: 1.126735 },
+      {id: 'GBP',  value: 0.876893 },
+      {id: 'INR',  value: 79.677056}
+    ];
+
+    const fromvalue: any = exchangeRates.find((exchange) => exchange.id == from)!.value;
+
+    const tovalue: any = exchangeRates.find((exchange) => exchange.id == to)!.value;
 
 
-  showResult()
-  {
-    console.log(this.fromCurr);
-    console.log(this.toCurr);
-    console.log(this.amount);
-    if(this.fromCurr !='' && this.toCurr != '')
-    {
-      this.show = true;
-    }
-    else
-    {
-      this.show = false;
-    }
+    let result: number = Math.round((value * tovalue) / fromvalue);
+    //result = parseInt(result.toFixed(2));
+    return result.toFixed(2);
+  
   }
+
 }
